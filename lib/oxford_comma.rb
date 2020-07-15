b40916684c2d97a8711c6b3d3d5759ae34bf1d9e
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 03:40:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   else
-    array.insert(-1, "and")
+    array.insert(-2, "and")
     array[0..-2].join(", ")
   end
 end
